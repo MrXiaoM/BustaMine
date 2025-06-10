e0199a0d23233f3207ccbd94fa3faacd20fe07fb
人間工作 <coolxiaom95@gmail.com>
2025-06-10 10:08:24 +0800

    add Pair replacement tools

---
+++ src/main/java/me/sat7/bustamine/utils/Pair.java
@@ -0,0 +1,114 @@
+package me.sat7.bustamine.utils;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pair<K, V> {
+    K key;
+    V value;
+
+    private Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public K key() {
+        return key;
+    }
+
+    public K left() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public void key(K key) {
+        this.key = key;
+    }
+
+    public void left(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public V value() {
+        return value;
+    }
+
+    public V right() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public void value(V value) {
+        this.value = value;
+    }
+
+    public void right(V value) {
+        this.value = value;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public <S, T> Pair<S, T> cast() {
+        return (Pair<S, T>) this;
+    }
+
+    public static <K, V> Pair<K, V> of(K key, V value) {
+        return new Pair<>(key, value);
+    }
+
+    public static List<String> replace(List<String> list, @Nullable Iterable<Pair<String, Object>> replacements) {
+        if (replacements == null) return new ArrayList<>(list);
+        List<String> result = new ArrayList<>();
+        for (String s : list) {
+            result.add(replace(s, replacements));
+        }
+        return result;
+    }
+
+    public static List<String> replace(List<String> list, Pair<String, Object> @Nullable [] replacements) {
+        if (replacements == null) return new ArrayList<>(list);
+        List<String> result = new ArrayList<>();
+        for (String s : list) {
+            result.add(replace(s, replacements));
+        }
+        return result;
+    }
+
+    public static String replace(String s, @Nullable Iterable<Pair<String, Object>> replacements) {
+        if (replacements == null) return s;
+        for (Pair<String, Object> replacement : replacements) {
+            if (replacement.key.startsWith("__internal__")) continue;
+            s = s.replace(replacement.key, String.valueOf(replacement.value));
+        }
+        return s;
+    }
+
+    public static String replace(String s, Pair<String, Object> @Nullable [] replacements) {
+        if (replacements == null) return s;
+        for (Pair<String, Object> replacement : replacements) {
+            if (replacement.key.startsWith("__internal__")) continue;
+            s = s.replace(replacement.key, String.valueOf(replacement.value));
+        }
+        return s;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public static <K, V> Pair<K, V>[] array(int length) {
+        return new Pair[length];
+    }
+}
